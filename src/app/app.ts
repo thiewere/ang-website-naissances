f2cd8058f033776b8f15be2dc3206067651577e7
thiewere <ousmanesow781@gmail.com>
2025-10-18 17:04:19 +0200

    Configuration de base

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,6 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('website-naissances');
+  subtitle = signal("Ousmane Sow");
+  typ = typeof (this.subtitle);
 }
