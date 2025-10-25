879fdd270b288a39df396a94f5dca6f80fe992e8
thiewere <ousmanesow781@gmail.com>
2025-10-25 13:51:49 +0200

    Enhance date formatting in declaration item and update status badge styles

--- src/app/src/components/declaration-item/declaration-item.ts
+++ src/app/src/components/declaration-item/declaration-item.ts
@@ -1,11 +1,12 @@
 import { Component, input } from '@angular/core';
 import { Declaration } from '../../models/Declaration';
 import { StatusBadge } from '../status-badge/status-badge';
+import { CommonModule, DatePipe } from '@angular/common';
 
 
 @Component({
   selector: 'app-declaration-item',
-  imports: [StatusBadge],
+  imports: [StatusBadge, CommonModule, DatePipe],
   templateUrl: './declaration-item.html',
   styleUrl: './declaration-item.scss'
 })
